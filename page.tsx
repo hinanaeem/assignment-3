a746ee92558fae99863d3d13cd8da7c7fad0d71f
hinanaeem <160482170+hinanaeem@users.noreply.github.com>
2024-10-02 00:45:17 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/Footer"
+import Header from "@/components/header"
+export default function Jobs() {
+    return(
+        <div>
+        <Header/>
+        <h1><b>Join Our Team</b></h1>
+        <p>We're Hiring!</p>
+        <p>we believe in creating an environment where creativity, collaboration, and innovation thrive. If you're passionate, motivated, and ready to contribute to a dynamic and growing team, we'd love to hear from you!</p>
+        <Footer/>
+        </div>
+    )
+}
