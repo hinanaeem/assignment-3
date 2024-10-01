bac7fad611e882caf6b4efe030f50e62b3877dbb
hinanaeem <160482170+hinanaeem@users.noreply.github.com>
2024-10-02 00:37:45 +0500

    Update Footer.tsx

--- Footer.tsx
+++ Footer.tsx
@@ -1,30 +1,5 @@
 import Link from "next/link"
-// export default function Footer() {
-//     return(
-//         <div className="footer">
-//             <ul className='footer-buttons'>
-   
-//    <a href="https://www.linkedin.com">LinkedIn</a>
-//    <a href="https://www.github.com">Github</a>
-//    <a href="https://www.facebook.com">Facebook</a>
-//    </ul>
 
-//         </div>
-//     )
-// }
-// const Footer = () => {
-//     return (
-//       <footer className="footer">
-//         <div className="footer-buttons">
-//           <button>Button 1</button>
-//           <button>Button 2</button>
-//           <button>Button 3</button>
-//         </div>
-//       </footer>
-//     );
-//   };
-  
-//   export default Footer;
 const Footer = () => {
   return (
     <footer className="footer">
